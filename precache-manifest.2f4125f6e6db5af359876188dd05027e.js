0e02e6a9acba7dd4017a6bcae9b896e11f5eeef2
Robert M <belamorris@gmail.com>
2019-04-10 14:28:28 -0600

    Updates

---
+++ precache-manifest.2f4125f6e6db5af359876188dd05027e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "8d41b99d8ca0a3adacde",
+    "url": "/clicky-game/static/js/main.caf02122.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8d41b99d8ca0a3adacde",
+    "url": "/clicky-game/static/css/main.a80c8a00.chunk.css"
+  },
+  {
+    "revision": "0018fd9f7246c1fead2a4d6dc21b9691",
+    "url": "/clicky-game/index.html"
+  }
+];
